28746771ea437a7c067b6ced3d0e3ebe876ac9cf
elson <elsonnezaj2005@gmail.com>
2023-09-03 10:31:40 +0200

    login form design

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import { Form, Input } from "antd";
+import { Button, Form, Input, Typography } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
 export default function Login() {
   return (
     <div className={styles.loginContainer}>
-      <Form layout="vertical">
-        <Input name="id" placeholder="ID/NIPT" type="text" />
-        <Input name="password" placeholder="Pasword" type="password" />
+      <div className={styles.userIcon}>
+        <UserOutlined />
+      </div>
+      <Form layout="vertical" className={styles.loginForm}>
+        <Input
+          name="id"
+          placeholder="ID / NIPT"
+          type="text"
+          className={styles.loginInput}
+        />
+        <Input
+          name="password"
+          placeholder="Pasword"
+          type="password"
+          className={styles.loginInput}
+        />
+        <div className={styles.submitRow}>
+          <Typography className={styles.forgotPassword}>
+            Forgot Password?
+          </Typography>
+          <Button shape="round" type="submit" className={styles.loginButton}>
+            Login
+          </Button>
+        </div>
       </Form>
     </div>
   );
