7dd91dd9857b8f2db3d22f6273862af75f0841f2
elson <elsonnezaj2005@gmail.com>
2023-09-11 17:11:07 +0200

    contact styles changed

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -10,6 +10,31 @@ export default function Contact() {
         <div className={styles.infoHeader}>
           <Typography className={styles.header}>Contact Info</Typography>
         </div>
+        <div className={styles.body}>
+          <div className={styles.contactBody}>
+            <Typography className={styles.bodyHeader}>Contact : </Typography>
+            <div className={styles.labelContainer}>
+              <Typography className={styles.label}>
+                +355 069 345 6789{" "}
+              </Typography>
+              <Typography className={styles.label}>
+                +355 067 444 6666{" "}
+              </Typography>
+              <Typography className={styles.label}>remus@info.com </Typography>
+            </div>
+          </div>
+          <div className={styles.contactBody}>
+            <Typography className={styles.bodyHeader}>Address : </Typography>
+            <div className={styles.labelContainer}>
+              <Typography className={styles.label}>
+                Plaza Tirana, Sheshi Skender Beu, Tirana 1001, Albania{" "}
+              </Typography>
+              <Typography className={styles.label}>
+                Shëtitorja Lasgush Poradeci, Tiranë, Albania{" "}
+              </Typography>
+            </div>
+          </div>
+        </div>
       </div>
       <div className={styles.appLogo}>
         <img
